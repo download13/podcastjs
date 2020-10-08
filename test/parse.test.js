fb62f7b3cda1774be60667375ec7f5e9cadd7044
Erin Dachtler <erin@erindachtler.me>
2020-10-08 15:15:17 -0700

    more accurate test names

--- test/parse.test.js
+++ test/parse.test.js
@@ -1,20 +1,20 @@
 const fs = require('fs/promises')
 const podcastJs = require('../bin/index')
 
-test.concurrent('full fetch test - Hello Internet', async () => {
+test.concurrent('parse test - Hello Internet', async () => {
   const feed = await getFixture('hello_internet_rss.xml')
   const podcast = await podcastJs.parsePodcast(feed)
   expect(podcast.title).toBe('Hello Internet')
   expect(podcast.image).toBe('https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng')
 })
 
-test.concurrent('full fetch test - Cortex', async () => {
+test.concurrent('parse test - Cortex', async () => {
   const feed = await getFixture('cortex_rss.xml')
   const podcast = await podcastJs.parsePodcast(feed)
   expect(podcast.title).toBe('Cortex')
 })
 
-test.concurrent('full fetch test, title check and date format - Serial', async () => {
+test.concurrent('parse test, title check and date format - Serial', async () => {
   const feed = await getFixture('serial_rss.xml')
   const podcast = await podcastJs.parsePodcast(feed)
   const firstEpisode = podcast.episodes.find(episode => episode.title === 'S01 Episode 01: The Alibi')
